d58e5c03dd2cb15e13688882fbec8a43ab116138
Divyansh Gupta <divyanshg809@gmail.com>
2025-10-31 15:30:05 +0530

    Update Google Tag Manager ID in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -112,9 +112,7 @@ export default function RootLayout({
       suppressHydrationWarning
       className={encoded_sans_expanded.className}
     >
-      <GoogleTagManager gtmId="GTM-PZ2MBPCK" />
-{/*       <GoogleTagManager gtmId="AW-17025867407" /> */}
-{/*       <GoogleAnalytics gaId="G-JGYN2BBC12" /> */}
+      <GoogleTagManager gtmId="GTM-W67WG7C9" />
       <body>
         <Header />
         <main>{children}</main>
